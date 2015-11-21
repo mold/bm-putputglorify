9a98249365a62d9b17681fd511d868651b4507bc
johntu <johntu@kth.se>
2015-11-21 01:24:48 +0100

    Added super nice physics with cannon.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,146 +3,149 @@ define(["THREE",
         "CameraMovement",
         "Airplane",
         "Skybox",
-        "IOHandler"],
-function(THREE,
-         RandomEngine,
-         CameraMovement,
-         Airplane,
-         Skybox,
-         IOHandler) {
+        "IOHandler"
+    ],
+    function(THREE,
+        RandomEngine,
+        CameraMovement,
+        Airplane,
+        Skybox,
+        IOHandler) {
 
-    // https://color.adobe.com/create/color-wheel
-    var colorScheme = [0x4914CC, 0x665199, 0x0040FF, 0xFFB740, 0xCC6F14];
-    var randomColorFromScheme = function () {
-        return colorScheme[Math.floor(RandomEngine.random()*colorScheme.length)];
-    };
-
-
-    var NewCube = function(color) {
-        var geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-        var material = new THREE.MeshBasicMaterial({
-            color: color,
-            side: THREE.DoubleSide
-        });
-
-        var cube = new THREE.Mesh(geometry, material);
-
-        return cube;
-    };
-
-    var NewAxis = function() {
-        var redCube = NewCube(0xee0000);
-        var blueCube = NewCube(0x00ee00);
-        var greenCube = NewCube(0x0000ee);
-        var whiteCube = NewCube(0xffffff);
-
-        redCube.position.x = 1;      // positive x-axis
-        blueCube.position.y = 1;     // positive y-axis
-        greenCube.position.z = 1;    // positive z-axis
-        // white is origo
-
-        var axis = new THREE.Object3D();
-
-        axis.add(redCube);
-        axis.add(blueCube);
-        axis.add(greenCube);
-        axis.add(whiteCube);
-
-        return axis;
-    };
-
-    var addLights = function(scene) {
-
-        var light = new THREE.AmbientLight(0x404040); // soft white light
-        scene.add(light);
-
-        var pointLight = new THREE.PointLight(0xeeeeee, 1, 0);
-        scene.add(pointLight);
-
-        pointLight.position.y = 5;
-        pointLight.position.z = -2;
-    };
+        // https://color.adobe.com/create/color-wheel
+        var colorScheme = [0x4914CC, 0x665199, 0x0040FF, 0xFFB740, 0xCC6F14];
+        var randomColorFromScheme = function() {
+            return colorScheme[Math.floor(RandomEngine.random() * colorScheme.length)];
+        };
 
 
-    (function () {
+        var NewCube = function(color) {
+            var geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+            var material = new THREE.MeshBasicMaterial({
+                color: color,
+                side: THREE.DoubleSide
+            });
 
-        var scene = new THREE.Scene();
-        var clock = new THREE.Clock();
-        var camera = new THREE.PerspectiveCamera(75,
-                                                 window.innerWidth /
-                                                 window.innerHeight,
-                                                 0.1,
-                                                 1000);
-        var renderer = new THREE.WebGLRenderer();
-        var iohandler = new IOHandler();
+            var cube = new THREE.Mesh(geometry, material);
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        var bgColor = randomColorFromScheme();
-        renderer.setClearColor( bgColor );
+            return cube;
+        };
 
-        document.body.appendChild(renderer.domElement);
+        var NewAxis = function() {
+            var redCube = NewCube(0xee0000);
+            var blueCube = NewCube(0x00ee00);
+            var greenCube = NewCube(0x0000ee);
+            var whiteCube = NewCube(0xffffff);
 
-        // TODO: figure out how fog works
-        scene.fog = new THREE.FogExp2( bgColor, 0.0025 );
+            redCube.position.x = 1; // positive x-axis
+            blueCube.position.y = 1; // positive y-axis
+            greenCube.position.z = 1; // positive z-axis
+            // white is origo
 
-        addLights(scene);
+            var axis = new THREE.Object3D();
 
-        var topObj = new THREE.Object3D();
+            axis.add(redCube);
+            axis.add(blueCube);
+            axis.add(greenCube);
+            axis.add(whiteCube);
 
-        var airplane = Airplane();
-        var axis = NewAxis();
-        var skybox = Skybox;
+            return axis;
+        };
 
-        topObj.add(airplane);
+        var addLights = function(scene) {
 
-        topObj.add(axis);
+            var light = new THREE.AmbientLight(0x404040); // soft white light
+            scene.add(light);
 
-        topObj.add(Skybox);
+            var pointLight = new THREE.PointLight(0xeeeeee, 1, 0);
+            scene.add(pointLight);
+
+            pointLight.position.y = 5;
+            pointLight.position.z = -2;
+        };
 
 
-        scene.add(topObj);
+        (function() {
 
-        camera.position.z = 5;
-        camera.position.y = 1;
+            var scene = new THREE.Scene();
+            var clock = new THREE.Clock();
+            var camera = new THREE.PerspectiveCamera(75,
+                window.innerWidth /
+                window.innerHeight,
+                0.1,
+                1000);
+            var renderer = new THREE.WebGLRenderer();
+            var iohandler = new IOHandler();
 
-        var resizeHandler = function( event ) {
-            camera.aspect = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix();
             renderer.setSize(window.innerWidth, window.innerHeight);
-        };
+            var bgColor = randomColorFromScheme();
+            renderer.setClearColor(bgColor);
 
-        window.addEventListener('resize',
-                                resizeHandler,
-                                false);
+            document.body.appendChild(renderer.domElement);
 
-        // Main render loop
-        window.paused = false;
-        var cm = new CameraMovement(camera);
-        cm.cameraPan(new THREE.Vector3(5, 2, 5));
+            // TODO: figure out how fog works
+            scene.fog = new THREE.FogExp2(bgColor, 0.0025);
 
-        var loop = function () {
+            addLights(scene);
 
-            if (window.paused) { return window.requestAnimationFrame( loop ); }
+            var topObj = new THREE.Object3D();
 
-            renderer.render( scene, camera );
+            var airplane = Airplane();
+            var axis = NewAxis();
+            var skybox = Skybox;
 
-            var deltaTime = clock.getDelta();
-            var ioRotationAndPos = iohandler.getRotationAndPosition();
-            var ioRotation = ioRotationAndPos[0];
-            var ioPosition = ioRotationAndPos[1];
+            topObj.add(airplane);
 
-            //console.log('%cRotation:', "color:green;", ioRotation);
+            topObj.add(axis);
 
-            airplane.quaternion.slerp(ioRotation, 0.1);
-            airplane.position.lerp(ioPosition, 0.1);
-            //airplane.material.uniforms.time.value += 10 * deltaTime;
+            topObj.add(Skybox);
 
-            cm.move(deltaTime);
 
-            return window.requestAnimationFrame( loop );
-        };
+            scene.add(topObj);
 
-        loop();
-    })();
+            camera.position.z = 5;
+            camera.position.y = 1;
 
-});
+            var resizeHandler = function(event) {
+                camera.aspect = window.innerWidth / window.innerHeight;
+                camera.updateProjectionMatrix();
+                renderer.setSize(window.innerWidth, window.innerHeight);
+            };
+
+            window.addEventListener('resize',
+                resizeHandler,
+                false);
+
+            // Main render loop
+            window.paused = false;
+            var cm = new CameraMovement(camera);
+            cm.cameraPan(new THREE.Vector3(5, 2, 5));
+
+            var loop = function() {
+
+                if (window.paused) {
+                    return window.requestAnimationFrame(loop);
+                }
+
+                renderer.render(scene, camera);
+
+                var deltaTime = clock.getDelta();
+                var ioRotationAndPos = iohandler.getRotationAndPosition();
+                var ioRotation = ioRotationAndPos[0];
+                var ioPosition = ioRotationAndPos[1];
+
+                //console.log('%cRotation:', "color:green;", ioRotation);
+
+                airplane.quaternion.slerp(ioRotation, 0.1);
+                airplane.position.lerp(ioPosition, 0.1);
+                //airplane.material.uniforms.time.value += 10 * deltaTime;
+
+                //cm.move(deltaTime);
+
+                return window.requestAnimationFrame(loop);
+            };
+
+            loop();
+        })();
+
+    });
