4251ce35896146e99a76494f5a78d8829988f666
Anton Erholt <anton.erholt@gmail.com>
2015-11-21 01:37:14 +0100

    Turtle in square world

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,124 +1,150 @@
 define(["THREE",
-        "RandomEngine",
-        "IOHandler",
-        "Sprite",
-        "WallSprite"],
-function(THREE,
-         RandomEngine,
-         IOHandler,
-         Sprite,
-         WallSprite) {
+    "RandomEngine",
+    "IOHandler",
+    "Sprite",
+    "WallSprite"
+], function(THREE,
+    RandomEngine,
+    IOHandler,
+    Sprite,
+    WallSprite) {
 
-       // https://color.adobe.com/create/color-wheel
-       var colorScheme = [0x4914CC, 0x665199, 0x0040FF, 0xFFB740, 0xCC6F14];
-            var randomColorFromScheme = function () {
-                return colorScheme[Math.floor(RandomEngine.random()*colorScheme.length)];
-            };
+    // https://color.adobe.com/create/color-wheel
+    var colorScheme = [0x4914CC, 0x665199, 0x0040FF, 0xFFB740, 0xCC6F14];
+    var randomColorFromScheme = function() {
+        return colorScheme[Math.floor(RandomEngine.random() * colorScheme.length)];
+    };
 
-            var addLights = function(scene) {
-                var light = new THREE.AmbientLight(0x404040); // soft white light
-                scene.add(light);
-            };
+    var addLights = function(scene) {
+        var light = new THREE.AmbientLight(0x404040); // soft white light
+        scene.add(light);
+    };
 
-            (function () {
+    (function() {
 
-                var scene = new THREE.Scene();
-                var clock = new THREE.Clock();
-                var camera = new THREE.PerspectiveCamera(75,
-                                                         window.innerWidth /
-                                                         window.innerHeight,
-                                                         0.1,
-                                                         1000);
-                var renderer = new THREE.WebGLRenderer();
-                var iohandler = new IOHandler();
+        var scene = new THREE.Scene();
+        var clock = new THREE.Clock();
+        var camera = new THREE.PerspectiveCamera(75,
+            window.innerWidth /
+            window.innerHeight,
+            0.1,
+            1000);
+        var renderer = new THREE.WebGLRenderer();
+        var iohandler = new IOHandler();
 
-                renderer.setSize(window.innerWidth, window.innerHeight);
-                var bgColor = 0x332222;//randomColorFromScheme();
-                renderer.setClearColor( bgColor );
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        var bgColor = 0x332222; //randomColorFromScheme();
+        renderer.setClearColor(bgColor);
 
-                document.body.appendChild(renderer.domElement);
+        document.body.appendChild(renderer.domElement);
 
-                addLights(scene);
+        addLights(scene);
 
-                var topObj = new THREE.Object3D();
+        var topObj = new THREE.Object3D();
 
-                var cube = new THREE.Mesh( new THREE.CubeGeometry(1,1,1), new THREE.MeshNormalMaterial() );
+        var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1), new THREE.MeshNormalMaterial());
 
-                var map = [
-                    [5,1,1,6],
-                    [4,0,0,2],
-                    [4,0,0,2],
-                    [8,3,3,7]
-                ];
+        var map = [
+            [1, 1, 1, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 1]
+        ];
 
-                var types = {
-                    1: "top",
-                    2: "right",
-                    3: "bottom",
-                    4: "left",
-                    5: "top-left",
-                    6: "top-right",
-                    7: "bottom-right",
-                    8: "bottom-left"
-                };
+        var tileMap = [
+            [5, 1, 1, 6],
+            [4, 0, 0, 2],
+            [4, 0, 0, 2],
+            [8, 3, 3, 7]
+        ];
 
-                var walls = [];
+        var types = {
+            1: "top",
+            2: "right",
+            3: "bottom",
+            4: "left",
+            5: "top-left",
+            6: "top-right",
+            7: "bottom-right",
+            8: "bottom-left"
+        };
 
-                var img = new Image();
-                img.src = "sprites/dawnlike/Objects/Wall.png";
-                img.onload = function() {
-                    for (var i=0;i<map.length;i++) {
-                        for (var j=0;j<map[i].length;j++) {
-                            if (map[i][j] != 0) {
-                                var wall = new WallSprite(img, types[map[i][j]]);
-                                wall._x = j-map[i].length/2;
-                                wall._y = map.length/2-i;
-                                wall.position.set(wall._x, wall._y, 0);
-                                wall.setSize(1,1);
-                                topObj.add(wall);
-                                walls.push(wall);
-                            }
-                        }
+        var walls = [];
+        // load wall image
+        var img = new Image();
+        img.src = "sprites/dawnlike/Objects/Wall.png";
+        img.onload = function() {
+            for (var i = 0; i < map.length; i++) {
+                for (var j = 0; j < map[i].length; j++) {
+                    if (map[i][j] != 0) {
+                        var wall = new WallSprite(img, types[tileMap[i][j]]);
+                        wall._x = j - map[i].length / 2 + 0.5;
+                        wall._y = map.length / 2 - i;
+                        wall.position.set(wall._x, wall._y, 0);
+                        wall.setSize(1, 1);
+                        topObj.add(wall);
+                        walls.push(wall);
                     }
-                };
+                }
+            }
+        };
 
-                //topObj.add(cube);
+        // load character
+        var character;
+        var img2 = new Image();
+        img2.src = "sprites/dawnlike/Characters/Reptile1.png";
+        img2.onload = function() {
+            character = new Sprite(img2, img2.width, img2.height, 16, 16);
+            character.setTile(8 * 11 + 1);
+            character.setSize(1, 1);
+            topObj.add(character);
+        };
 
-                scene.add(topObj);
+        //topObj.add(cube);
 
-                camera.position.z = 5;
-                camera.position.y = 0;
+        scene.add(topObj);
 
-                var resizeHandler = function( event ) {
-                    camera.aspect = window.innerWidth / window.innerHeight;
-                    camera.updateProjectionMatrix();
-                    renderer.setSize(window.innerWidth, window.innerHeight);
-                };
+        camera.position.z = 5;
+        camera.position.y = 0;
 
-                window.addEventListener('resize',
-                                        resizeHandler,
-                                        false);
+        var resizeHandler = function(event) {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        };
 
-                // Main render loop
-                window.paused = false;
+        window.addEventListener('resize',
+            resizeHandler,
+            false);
 
+        // Main render loop
+        window.paused = false;
 
-                var loop = function () {
+        var ang = 0;
+        var loop = function() {
 
-                    if (window.paused) { return window.requestAnimationFrame( loop ); }
+            if (window.paused) {
+                return window.requestAnimationFrame(loop);
+            }
 
-                    renderer.render( scene, camera );
+            renderer.render(scene, camera);
 
-                    var deltaTime = clock.getDelta();
+            var deltaTime = clock.getDelta();
 
-                    for (var i=0; i<walls.length; i++) {
-                        walls[i].position.set(walls[i]._x+Math.random()*deltaTime*2, walls[i]._y+Math.random()*deltaTime*2, 0);
-                    }
+            for (var i = 0; i < walls.length; i++) {
+                walls[i].position.set(walls[i]._x + Math.random() * deltaTime * 2, walls[i]._y + Math.random() * deltaTime * 2, 0);
+            }
 
-                    return window.requestAnimationFrame( loop );
-                };
+            if (character) {
+                character.position.set(Math.cos(ang) * 0.5 - 0.5, Math.sin(ang) * 0.5 + 0.5, 0);
+                character.rotation.setFromQuaternion(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), ang));
+            }
+            ang += Math.PI * deltaTime;
 
-                loop();
-            })();
+            return window.requestAnimationFrame(loop);
+        };
 
-           });
+        loop();
+    })();
+
+});
